7d731a492359a1995aa855db8832b8e3d0748303
Ramanpreet6262 <ramanpreet6262@gmail.com>
2020-02-08 14:32:45 +0530

    added register component

--- bootcamper/src/App.js
+++ bootcamper/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import Landing from './Components/Landing_Page/Landing';
+// import Landing from './Components/Landing_Page/Landing';
+// import Register from './Components/Register/Register';
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Landing />
     </div>
   );
 };
